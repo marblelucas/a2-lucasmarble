04f161089fb82c696de7611ec010a6d5af4bc367
marblelucas <lucasehmarble@gmail.com>
2025-09-10 18:38:12 -0400

    All Changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,225 @@
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 
 const submit = async function( event ) {
+  event.preventDefault()
   // stop form submission from trying to load
   // a new .html page for displaying results...
   // this was the original browser behavior and still
   // remains to this day
-  event.preventDefault()
+
+
   
-  const input = document.querySelector( "#yourname" ),
-        json = { yourname: input.value },
-        body = JSON.stringify( json )
+  if (stormname.value == "" || year.value == ""){
+    alert("You forgot to put in the name and year")
+  }
 
-  const response = await fetch( "/submit", {
-    method:"POST",
-    body 
-  })
+  else{
 
-  const text = await response.text()
+    let oldTable = document.getElementById("dataset");
+    let parentElement = oldTable.parentNode;
+    parentElement.removeChild(oldTable);
+    
+    const stormname = document.querySelector( "#stormname" ),
+          year = (document.querySelector( "#year" )),
+          windspeed = (document.querySelector( "#windspeed" )),
+          airpressure = (document.querySelector( "#airpressure" ))
+    
+    const dataset = document.createElement('table');
 
-  console.log( "text:", text )
+    const data = {
+      Name: stormname.value,
+      Year: year.value,
+      Windspeed: windspeed.value,
+      Airpressure: airpressure.value
+    };
+    
+    const body = JSON.stringify(data)
+    
+    
+
+    const response = await fetch( "/submit", {
+      method:"POST",
+      body 
+    }).then(function(response) {return response.json();})
+    .then(function(json) {
+      
+    
+
+      const categories = document.createElement('thead');
+      const hurricanes = document.createElement('tbody');
+
+      const catRow = document.createElement('tr');
+      Object.keys(json[0]).forEach(key => {
+        const cat = document.createElement('th');
+        cat.textContent = key;
+        if (cat.textContent === "Windspeed"){
+          cat.textContent = "Wind Speed (mph, 1-minute sustained)"
+        }
+        if (cat.textContent === "Airpressure"){
+          cat.textContent = "Air Pressure (mbar)"
+        }
+        catRow.appendChild(cat);
+      });
+      categories.appendChild(catRow);
+
+      json.forEach( item => {
+        let wind = 0;
+        const hurricane = document.createElement('tr');
+        Object.values(item).forEach(value => {
+          wind++;
+          const property = document.createElement('td');
+          property.textContent = value;
+          hurricane.appendChild(property);
+          if (wind == 3){
+            if (value >= 157){
+              hurricane.style.backgroundColor = 'pink';
+            }
+            else if (value >= 130){
+              hurricane.style.backgroundColor = 'red';
+            }
+            else if (value >= 111){
+              hurricane.style.backgroundColor = 'orange';
+            }
+            else if (value >= 96){
+              hurricane.style.backgroundColor = 'yellow';
+            }
+            else if (value >= 74){
+              hurricane.style.backgroundColor = 'white';
+            }
+            else if (value >= 39){
+              hurricane.style.backgroundColor = 'cyan';
+            }
+            else{
+              hurricane.style.backgroundColor = 'blue';
+            }
+          }
+        })
+        hurricanes.appendChild(hurricane);
+
+      })
+
+      dataset.appendChild(categories);
+      dataset.appendChild(hurricanes);
+
+      dataset.id = "dataset";
+
+      document.body.appendChild(dataset);
+
+    })
+  }
+
+}
+
+const deletion = async function( event ) {
+  event.preventDefault()
+  // stop form submission from trying to load
+  // a new .html page for displaying results...
+  // this was the original browser behavior and still
+  // remains to this day
+
+
+  
+  if (stormname.value == "" || year.value == ""){
+    alert("You forgot to put in the name and year")
+  }
+
+  else{
+
+    let oldTable = document.getElementById("dataset");
+    let parentElement = oldTable.parentNode;
+    parentElement.removeChild(oldTable);
+    
+    const stormname = document.querySelector( "#stormname" ),
+          year = (document.querySelector( "#year" ))
+    
+    const dataset = document.createElement('table');
+
+    const data = {
+      Name: stormname.value,
+      Year: year.value,
+    };
+    
+    const body = JSON.stringify(data)
+    
+    
+
+    const response = await fetch( "/delete", {
+      method:"POST",
+      body 
+    }).then(function(response) {return response.json();})
+    .then(function(json) {
+      
+      
+
+      const categories = document.createElement('thead');
+      const hurricanes = document.createElement('tbody');
+
+      const catRow = document.createElement('tr');
+      Object.keys(json[0]).forEach(key => {
+        const cat = document.createElement('th');
+        cat.textContent = key;
+        if (cat.textContent === "Windspeed"){
+          cat.textContent = "Wind Speed (mph, 1-minute sustained)"
+        }
+        if (cat.textContent === "Airpressure"){
+          cat.textContent = "Air Pressure (mbar)"
+        }
+        catRow.appendChild(cat);
+      });
+      categories.appendChild(catRow);
+
+      json.forEach( item => {
+        let wind = 0;
+        const hurricane = document.createElement('tr');
+        Object.values(item).forEach(value => {
+          wind++;
+          const property = document.createElement('td');
+          property.textContent = value;
+          hurricane.appendChild(property);
+          if (wind == 3){
+            if (value >= 157){
+              hurricane.style.backgroundColor = 'pink';
+            }
+            else if (value >= 130){
+              hurricane.style.backgroundColor = 'red';
+            }
+            else if (value >= 111){
+              hurricane.style.backgroundColor = 'orange';
+            }
+            else if (value >= 96){
+              hurricane.style.backgroundColor = 'yellow';
+            }
+            else if (value >= 74){
+              hurricane.style.backgroundColor = 'white';
+            }
+            else if (value >= 39){
+              hurricane.style.backgroundColor = 'cyan';
+            }
+            else{
+              hurricane.style.backgroundColor = 'blue';
+            }
+          }
+        })
+        hurricanes.appendChild(hurricane);
+
+      })
+
+      dataset.appendChild(categories);
+      dataset.appendChild(hurricanes);
+
+      dataset.id = "dataset";
+
+      document.body.appendChild(dataset);
+
+    })
+  }
+
 }
 
 window.onload = function() {
    const button = document.querySelector("button");
   button.onclick = submit;
+   const removal = document.querySelector("#delete");
+  removal.onclick = deletion;
+   
 }
--- server.improved.js
+++ server.improved.js
@@ -8,11 +8,7 @@ const http = require( "http" ),
       dir  = "public/",
       port = 3000
 
-const appdata = [
-  { "model": "toyota", "year": 1999, "mpg": 23 },
-  { "model": "honda", "year": 2004, "mpg": 30 },
-  { "model": "ford", "year": 1987, "mpg": 14} 
-]
+const appdata = []
 
 const server = http.createServer( function( request,response ) {
   if( request.method === "GET" ) {
@@ -32,20 +28,71 @@ const handleGet = function( request, response ) {
   }
 }
 
+const sshws = function( mph ) {
+  if (mph >= 157){
+    return "Category 5 Major Hurricane";
+  }
+  else if (mph >= 130){
+    return "Category 4 Major Hurricane";
+  }
+  else if (mph >= 111){
+    return "Category 3 Major Hurricane";
+  }
+  else if (mph >= 96){
+    return "Category 2 Hurricane";
+  }
+  else if (mph >= 74){
+    return "Category 1 Hurricane";
+  }
+  else if (mph >= 39){
+    return "Tropical Storm";
+  }
+  else{
+    return "Tropical Depression";
+  }
+}
+
 const handlePost = function( request, response ) {
   let dataString = ""
+
+  
+  let url = request.url.slice ( 1 );
 
   request.on( "data", function( data ) {
       dataString += data 
   })
 
   request.on( "end", function() {
-    console.log( JSON.parse( dataString ) )
+    if(url == "submit"){
+      let add = JSON.parse( dataString );
+      add.Airpressure = parseInt(add.Airpressure)
+      add.Windspeed = parseInt(add.Windspeed)
+      add.Year = parseInt(add.Year)
+      console.log(add)
+      add.Category = sshws(add.Windspeed)
+      let AddedIndex = appdata.findIndex(function(element) {
+        return element.Name === add.Name && element.Year === add.Year;
+      });
 
-    // ... do something with the data here!!!
-
-    response.writeHead( 200, "OK", {"Content-Type": "text/plain" })
-    response.end("test")
+      if(AddedIndex == -1){
+        appdata.push(add);
+      }
+      else{
+        appdata[AddedIndex] = add;
+      }
+    }
+    else if(url == "delete"){
+      let subtract = JSON.parse( dataString );
+      subtract.Year = parseInt(subtract.Year)
+      appdata.forEach((storm, index) => {
+        if (subtract.Name == storm.Name && subtract.Year == storm.Year){
+          appdata.splice(index, 1);
+        }
+        }
+      )
+    }
+    response.writeHead( 200, "OK", {"Content-Type": "application/json" })
+    response.end(JSON.stringify(appdata))
   })
 }
 
